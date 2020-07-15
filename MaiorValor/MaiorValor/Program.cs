b42169d42ed51f901464ea3fcad69ad1268344d5
Marcio Alves de Oliveira <64289932+madeoliveira@users.noreply.github.com>
2020-07-14 21:41:46 -0300

    Determina qual o maior valor entre os quatro valores informado.

---
+++ MaiorValor/MaiorValor/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MaiorValor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, maior=0; 
+            Console.WriteLine("Determina o maior número");
+            Console.Write("Informe o rimeiro valor: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            maior = n;
+            Console.Write("Informe o segundo valor: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            if (n > maior)
+            {
+                maior = n;
+            }
+            Console.Write("Informe o terceiro valor: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            if (n > maior)
+            {
+                maior = n;
+            }
+            Console.Write("Informe o quarto valor: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            if (n > maior)
+            {
+                maior = n;
+            }
+            Console.WriteLine("O maior valor informado é : "+maior);
+            Console.ReadLine();
+        }
+    }
+}
