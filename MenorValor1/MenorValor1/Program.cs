d57ba4c283038cf3cef3ba87a91d7fe440a28e0f
Marcio Alves de Oliveira <64289932+madeoliveira@users.noreply.github.com>
2020-07-15 20:14:51 -0300

    Determina o menor valor informado usando while

---
+++ MenorValor1/MenorValor1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MaiorValor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1, menor = 0, n = 0;
+            Console.WriteLine("Determina o menor valor. \n");
+            Console.Write("Informe o " + i + " valor: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            menor = n;
+
+            while (i <= 9)
+            {
+                i += 1;
+
+
+                Console.Write("Informe o " + i + " valor: ");
+                n = Convert.ToInt32(Console.ReadLine());
+
+                if (menor > n)
+                {
+                    menor = n;
+                }
+
+
+            }
+            Console.WriteLine("\nO menor valor informado é : " + menor);
+            Console.ReadLine();
+        }
+    }
+}
