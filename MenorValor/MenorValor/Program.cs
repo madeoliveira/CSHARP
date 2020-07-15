20a2979b87196bb98d80e9cc585e34227193efa5
Marcio Alves de Oliveira <64289932+madeoliveira@users.noreply.github.com>
2020-07-14 21:48:59 -0300

    Determina qual o menor valor entre os quatro valores informado.

---
+++ MenorValor/MenorValor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace MenorValor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, maior = 0;
+            Console.WriteLine("Determina o menor valor");
+            Console.Write("Informe o rimeiro valor: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            maior = n;
+            Console.Write("Informe o segundo valor: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            if (n < maior)
+            {
+                maior = n;
+            }
+            Console.Write("Informe o terceiro valor: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            if (n < maior)
+            {
+                maior = n;
+            }
+            Console.Write("Informe o quarto valor: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            if (n < maior)
+            {
+                maior = n;
+            }
+            Console.WriteLine("O menor valor informado é : " + maior);
+            Console.ReadLine();
+        }
+    }
+}
+
