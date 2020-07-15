f4d27ccf788412da9e108d1943c3d86e3bd6a8b2
Marcio Alves de Oliveira <64289932+madeoliveira@users.noreply.github.com>
2020-07-14 21:07:50 -0300

    Inverter números de 3 dígitos

---
+++ InverterNumeros/InverterNumeros/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InverterNumeros
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ni = 0, c = 0, d = 0, u = 0;
+            Console.WriteLine("Programa que mostra o número informado invertido.");
+            Console.Write("Informe um número de 3 dígitos: ");
+            ni = Convert.ToInt32(Console.ReadLine());
+            c = ni / 100;
+            d = (ni % 100) / 10;
+            u = ni % 10;
+            Console.WriteLine("O número informado invertido fica: " + u + d + c);
+            Console.ReadKey();
+
+        }
+    }
+}
