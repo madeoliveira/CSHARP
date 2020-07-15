d71345b6ec51ee0f558d5953911fba00122f0f99
Marcio Alves de Oliveira <64289932+madeoliveira@users.noreply.github.com>
2020-07-15 20:09:53 -0300

    Determina o maior valor entre os 10 valores informado. Usando while.

---
+++ MaiorValor1/MaiorValor1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MaiorValor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1, maior = 0, n=0; 
+            Console.WriteLine("Determina o maior valor. \n");
+            Console.Write("Informe o " +i+" valor: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            maior = n;
+           
+            while (i <= 9)
+            {
+                i += 1;
+                
+               
+               Console.Write("Informe o " + i +" valor: ");
+                n = Convert.ToInt32(Console.ReadLine());
+
+                if (maior < n)
+                {
+                    maior = n;
+                }
+
+               
+            }
+            Console.WriteLine("\nO maior valor informado é : "+maior);
+            Console.ReadLine();
+        }
+    }
+}
