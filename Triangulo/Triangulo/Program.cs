5af1abdfb622221cc3e7acbbe2d0a22894cbc90f
Marcio Alves de Oliveira <64289932+madeoliveira@users.noreply.github.com>
2020-07-14 22:07:21 -0300

    Verifica se três valores informados representam os dados de um triângulo

---
+++ Triangulo/Triangulo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Triangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=0, b=0, c=0;
+            String msg = "Não representam os lados de um triângulo.";
+
+            Console.WriteLine("Verifica se três valores informados representam os dados de um triângulo.");
+            Console.Write("Informe o valor do lado A do triângulo: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Informe o valor do lado B do triângulo: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Informe o valor do lado C do triângulo: ");
+            c = Convert.ToInt32(Console.ReadLine());
+            if (a < b + c)
+            {
+                if (b < a + c)
+                {
+                    if (c < b + c)
+                    {
+                        msg = "Representam os lados de um triãngulo. ";
+                    }
+                }
+            }
+            Console.WriteLine(msg);
+            Console.ReadKey();
+        }
+    }
+}
