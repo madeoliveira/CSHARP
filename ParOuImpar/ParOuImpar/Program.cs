e490e242966c974bb1fea4718c25a9e531a15895
Marcio Alves de Oliveira <64289932+madeoliveira@users.noreply.github.com>
2020-07-14 21:21:39 -0300

    Verifica se o número é par ou impar.

---
+++ ParOuImpar/ParOuImpar/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ParOuImpar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0, r = 0;
+            Console.WriteLine("Verificar se o número informado é par ou impar");
+            Console.Write("Informe um número: ");
+            n = Convert.ToInt32(Console.ReadLine());
+            r = n % 2;
+            if(r ==0 )
+            {
+                Console.WriteLine("O número informado é par");
+            }
+            else
+            {
+                Console.WriteLine("O número informado é impar");
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
