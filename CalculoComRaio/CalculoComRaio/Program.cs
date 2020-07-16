13bbce8c8f7ed968cd96270eb8897390fdc9a39e
Marcio Alves de Oliveira <64289932+madeoliveira@users.noreply.github.com>
2020-07-16 15:43:58 -0300

    Calcula o diâmetro, a área e a circunferência de um circulo.

---
+++ CalculoComRaio/CalculoComRaio/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculoComRaio
+{
+    class Program
+    {
+        static Double calculaDiametro(Double r)
+        {
+            return 2 * r;
+        }
+        static Double calculaArea(Double r)
+        {
+            return 3.14 * (r * r);
+        }
+        static Double calculaCircunferencia(Double r)
+        {
+            return 2*3.14*r;
+        }
+        static void Main(string[] args)
+        {
+            Double raio = 0;
+            Double area = 0;
+            Console.WriteLine("Calcula o diâmetro, a área e a circunferência de um circulo");
+            Console.Write("Informe o raio: ");
+            raio = Convert.ToDouble(Console.ReadLine());
+            area = calculaArea(raio);
+            Console.WriteLine("Area: " + area);
+            Console.WriteLine("Diâmentro: " + calculaDiametro(raio));
+            Console.WriteLine("Circunferência: " + calculaCircunferencia(raio));
+            Console.ReadKey();
+
+        }
+    }
+}
