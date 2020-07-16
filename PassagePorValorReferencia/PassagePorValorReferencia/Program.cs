87144431fe2a237eae4a98121b9fe948d2eaed06
Marcio Alves de Oliveira <64289932+madeoliveira@users.noreply.github.com>
2020-07-16 16:21:18 -0300

    PAssagem de parâmetros por valor e por referência.

---
+++ PassagePorValorReferencia/PassagePorValorReferencia/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PassagePorValorReferencia
+{
+    class Program
+    {
+        static void passagemPorValor(int a)
+        {
+            a = 10;
+        }
+        static void passagemPorReferencia(ref int a)
+        {
+            a = 10;
+        }
+        static void Main(string[] args)
+        {
+            int n = 5;
+            passagemPorValor(n);
+            Console.WriteLine("Passagem de parâmentro por valor "+ n);
+            passagemPorReferencia(ref n);
+            Console.WriteLine("Passagem por reerência " + n);
+            Console.ReadKey();
+
+        }
+    }
+}
