82304fa0a76b871cc62a711260bf8e69c03b97dc
Marcio Alves de Oliveira <64289932+madeoliveira@users.noreply.github.com>
2020-07-16 14:20:24 -0300

    Converte um número decimal para bínario

---
+++ NumeroBinario/NumeroBinario/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumeroBinario
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = 0;
+            string numBinario = "";
+
+            Console.WriteLine("Converte um número decimal para bínario");
+            Console.Write("Informe um número: ");
+            num = Convert.ToInt32(Console.ReadLine());
+            while (num / 2 >= 1)
+            {
+                numBinario = Convert.ToString(num % 2) + numBinario;
+                num = num / 2;
+            }
+            numBinario = Convert.ToString(num) + numBinario;
+            Console.WriteLine("A conversão do número informado é: " + numBinario);
+            Console.ReadKey();
+        }
+    }
+}
