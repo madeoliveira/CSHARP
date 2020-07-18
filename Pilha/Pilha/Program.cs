db393655857e33c3724ceab49a481a87810c3543
Marcio Alves de Oliveira <64289932+madeoliveira@users.noreply.github.com>
2020-07-18 12:32:15 -0300

    Pilha (LIFO (Last-In-First-Out)).

---
+++ Pilha/Pilha/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pilha
+{
+    class Program
+    {
+        static Boolean Empilhar(int[] pilha, ref int topo, int dado)
+        {
+            Boolean retorno = false;
+            if (topo<10)
+            {
+                pilha[topo] = dado;
+                topo = topo + 1;
+                retorno = true;
+            }
+            return retorno;
+
+           
+        }
+
+        static Boolean Desempilhar(int[] pilha, ref int topo, ref int dado)
+        {
+            Boolean retorno = false;
+            if (topo > 0)
+            {
+                topo = topo - 1;
+                dado = pilha[topo];
+                retorno = true;
+            }
+            return retorno;
+        }
+          static void Main(string[] args)
+            {
+
+            }
+    }
+}
