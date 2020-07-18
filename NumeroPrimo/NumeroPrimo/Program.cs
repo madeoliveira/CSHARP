bf1825471913f90bf402a2e2f1b2dacd6b8d7f80
Marcio Alves de Oliveira <64289932+madeoliveira@users.noreply.github.com>
2020-07-17 21:22:26 -0300

    Exemplo, calcula se o número é primo.

---
+++ NumeroPrimo/NumeroPrimo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace NumeroPrimo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 0, r = 0, cont=0;
+;
+            Console.WriteLine("Número Primos");
+            Console.Write("Informe um número: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = n; i >0; i--)
+            {
+                if (n % i == 0)
+                {
+                    cont++;
+                }
+            }
+
+            if (cont==2)
+            {
+                Console.WriteLine("O número " + n + " é um número primo.");
+
+            }
+            else
+            {
+                Console.WriteLine("O número " + n + " não é número primo.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ TabelaDePontos/TabelaDePontos/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TabelaDePontos
+{
+    class Program
+    {
+
+        static int ExibeMenu()
+        {
+            int op=0;
+            Console.Clear();
+            Console.WriteLine("Tabela de pontos dos times Brasileiros");
+            Console.WriteLine("1 - Inserir Time");
+            Console.WriteLine("2 - Excluir Time");
+            Console.WriteLine("3 - Alterar Time");
+            Console.WriteLine("4 - Localizar Time");
+            Console.WriteLine("100 - Sair");
+            Console.WriteLine("O que deseja fazer? ");
+            op = Convert.ToInt32(Console.ReadLine());
+
+
+
+            return op;
+
+        }
+        static void Main(string[] args)
+        {
+            String[] nomes = new String[100];
+            int[] codigos = new int[100];
+            int[] pontos = new int[100];
+            int tl = 0; int lastcod = 0, op =0;
+
+            while (op != 10)
+            {
+                op = ExibeMenu();
+                if (op == 1)
+                {
+                    Console.WriteLine("Inserir ");
+                }
+           
+                if (op == 2)
+                {
+
+                }
+             
+                if (op == 3)
+                {
+
+                }
+               
+                if (op == 4)
+                {
+
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
